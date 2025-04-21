c143fdee70dbaff01e6ca3894ef4aaf534b1ed01
somecreater <127456520+somecreater@users.noreply.github.com>
2025-04-21 16:43:46 +0900

    빈 폴더에 .gitkeep 추가(무시)

---
+++ src/main/java/com/petservice/main/business/database/entity/PetBusinessType.java
@@ -0,0 +1,28 @@
+package com.petservice.main.business.database.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "pet_business_type")
+@Entity
+@Getter
+@Setter
+public class PetBusinessType {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "type_name")
+  private String typeName;
+
+  @Column(columnDefinition = "TEXT")
+  private String description;
+
+  @OneToMany(mappedBy = "petBusinessType")
+  private List<PetBusiness> petBusinessList = new ArrayList<>();
+}
---
+++ src/main/java/com/petservice/main/user/database/entity/Pet.java
@@ -0,0 +1,42 @@
+package com.petservice.main.user.database.entity;
+
+import com.petservice.main.common.database.entity.TimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Table(name = "pet")
+@Entity
+@Getter
+@Setter
+public class Pet extends TimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String name;
+
+  @Column
+  private String species;
+
+  @Column
+  private String breed;
+
+  @Column(name = "birth_date")
+  private LocalDate birthDate;
+
+  @Column(name = "health_info", columnDefinition = "TEXT")
+  private String healthInfo;
+
+  @Enumerated(EnumType.STRING)
+  @Column
+  private Gender gender;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+}
