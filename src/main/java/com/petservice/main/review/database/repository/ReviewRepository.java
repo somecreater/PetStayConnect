4ac35af693f798dc9a4e384dffd24b1ae71d3758
kyungmin <kyungmin@example.com>
2025-05-08 19:04:11 +0900

    feat: review CRUD 기능 구현

--- src/main/java/com/petservice/main/review/database/repository/ReviewRepository.java
+++ src/main/java/com/petservice/main/review/database/repository/ReviewRepository.java
@@ -4,6 +4,10 @@ import com.petservice.main.review.database.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    boolean existsByReservationId(Long reservationId);
+    List<Review> findAllByUser_UserLoginId(String userLoginId);
 }
