6d71bf5626ddc135f8f015048b4a2a3a6b6f2ae0
somecreater <127456520+somecreater@users.noreply.github.com>
2025-05-13 21:21:04 +0900

    일부 애완동물 사업자 방 관련 컴포넌트 구현

---
+++ frontend/src/business/Form/RoomRegisterForm.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { useUser } from "../../common/Context/UserContext";
+import ApiService from "../../common/Api/ApiService";
+
+function RoomRegisterForm(props){
+
+  const {user} =useUser();
+  const [newroom, setNewroom]=useState({
+    id: null,
+    petBusinessId: user.petBusinessDTO.id,
+    petBusinessRegisterNumber: user.petBusinessDTO.registrationNumber,
+    roomType: '',
+    description: '',
+    roomCount: 0,
+    reservationDTOList: null
+  });
+
+  const handleChange = (e) => {
+    const {name, value} = e;
+    setNewroom(prev => ({ ...prev, [name]: value }));
+  }
+
+  const handleSubmit = async ()=> {
+    e.preventDefault();
+
+    try{
+      const response = await ApiService.businessroom.register(newroom.petBusinessId,newroom);
+      const data= response.data;
+
+      if(data.result){
+        const room=data.room;
+        alert("새로운 방이 추가되었습니다.\n" 
+          + "새로운 방 타입: " + room.roomType 
+          + "새로운 방 설명: " + room.description
+          + "새로운 방의 수: " + room.roomCount);
+      }else{
+        const message=data.message;
+        alert(message);
+      }
+
+    }catch(err){
+      console.err(err);
+      alert('새로운 방 등록에 실패했습니다. 다시 시도해보세요');
+    }
+
+  }
+
+  return (
+    <div className="container py-4">
+      <form onSubmit={handleSubmit} className="border p-4 rounded bg-light">
+        <h4 className="mb-4">새로운 방 등록</h4>
+
+        <div className="mb-3">
+          <label className="form-label">타입</label>
+          <input
+            className="form-control"
+            type="text"
+            name="roomType"
+            value={newroom.roomType}
+            placeholder="방 타입"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="row g-3">   
+          <div className="col-md-6">
+            <label className="form-label">설명</label>
+            <input
+              className="form-control"
+              type="text"
+              name="description"
+              value={newroom.description}
+              placeholder="방 설명"
+              onChange={handleChange}
+          />
+          </div>
+          
+          <div className="col-md-6">
+            <label className="form-label">방 수</label>
+            <input
+              className="form-control"
+              type="number"
+              name="roomCount"
+              value={newroom.roomCount}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default RoomRegisterForm;
--- frontend/src/pay/Component/PayList.jsx
+++ frontend/src/pay/Component/PayList.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Pay from "./Pay";
 
-function PayList({List ,isDelete, onDelete, isBusiness}){
+function PayList({List ,isDelete, onDelete, isBusiness, onSelected}){
+
+  const handleClick= (pay)=>{
+    onSelected(pay);
+  }
+  
   if(!List || List.length === 0){
     return <div className="text-center text-muted">결제 내역이 없습니다.</div>
-  
-}
+  }
   return (
     <div className="row gy-4">
       {isBusiness === true ? <h5>결제 내역(사업자)</h5>:
       <h5>결제 내역</h5>
       }
       {List.map(pay => (
-        <div key={pay.id} className="col-12">
+        <div key={pay.id} className="col-12" onClick={handleClick(pay)}>
           <Pay PayDTO={pay}/>
           {isDelete && (
             <button
