41d340540f27faf8c53c22b165314a4cd019264d
Suu-a <sapark1121@gmail.com>
2025-05-15 14:05:51 +0900

    Add heart icon to Bookmark, downgrade react-icons to 5.4.0 for compatibility (북마크 데이터가 실제로 있을 때만 하트가 보임)

--- frontend/src/bookmark/Bookmark.jsx
+++ frontend/src/bookmark/Bookmark.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { FaHeart } from 'react-icons/fa'
 
 // 북마크 하나를 보여주는 컴포넌트
 export default function Bookmark({ item }) {
   return (
     <li style={{ marginBottom: '1em', display: 'flex', alignItems: 'center' }}>
+         <FaHeart color="#f44336" size={24} style={{marginRight: 12}} />
       <img src={item.image} alt={item.name} width={60} height={60} style={{ borderRadius: 8, marginRight: 16 }} />
       <span>{item.name}</span>
     </li>
